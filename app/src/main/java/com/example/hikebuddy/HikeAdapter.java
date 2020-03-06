46e953b0d7ac88f143e132f74c2d4571c36372ae
Nathan Onaka <nateonaka@gmail.com>
2020-03-05 23:25:12 -1000

    RecyclerView and CardView

---
+++ app/src/main/java/com/example/hikebuddy/HikeAdapter.java
@@ -0,0 +1,104 @@
+package com.example.hikebuddy;
+
+import android.content.Context;
+import android.content.Intent;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.hikebuddy.R;
+
+import java.util.ArrayList;
+
+import static android.view.KeyCharacterMap.load;
+
+/***
+ * The adapter class for the RecyclerView, contains the Hike data.
+ */
+class HikeAdapter extends RecyclerView.Adapter<HikeAdapter.ViewHolder>  {
+
+    // Member variables.
+    private ArrayList<Hike> mHikeData;
+    private Context mContext;
+
+    /**
+     * Constructor that passes in the Hike data and the context.
+     *
+     * @param HikeData ArrayList containing the Hike data.
+     * @param context Context of the application.
+     */
+    HikeAdapter(Context context, ArrayList<Hike> HikeData) {
+        this.mHikeData = HikeData;
+        this.mContext = context;
+    }
+
+
+    /**
+     * Required method for creating the viewholder objects.
+     *
+     * @param parent The ViewGroup into which the new View will be added
+     *               after it is bound to an adapter position.
+     * @param viewType The view type of the new View.
+     * @return The newly created ViewHolder.
+     */
+    @Override
+    public HikeAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(mContext).
+                inflate(R.layout.hike_item, parent, false));
+    }
+
+    /**
+     * Required method that binds the data to the viewholder.
+     *
+     * @param holder The viewholder into which the data should be put.
+     * @param position The adapter position.
+     */
+    @Override
+    public void onBindViewHolder(HikeAdapter.ViewHolder holder, int position) {
+        // Get current Hike.
+        Hike currentHike = mHikeData.get(position);
+
+        holder.mTitleText.setText(currentHike.getTitle());
+        holder.mInfoText.setText(currentHike.getInfo());
+        holder.mHikeImage.setImageResource(mHikeData.get(position).getImageResource());
+    }
+
+    /**
+     * Required method for determining the size of the data set.
+     *
+     * @return Size of the data set.
+     */
+    @Override
+    public int getItemCount() {
+        return mHikeData.size();
+    }
+
+
+    /**
+     * ViewHolder class that represents each row of data in the RecyclerView.
+     */
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        // Member Variables for the TextViews
+        private TextView mTitleText;
+        private TextView mInfoText;
+        private ImageView mHikeImage;
+
+        /**
+         * Constructor for the ViewHolder, used in onCreateViewHolder().
+         *
+         * @param itemView The rootview of the list_item.xml layout file.
+         */
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            // Initialize the views.
+            mTitleText = itemView.findViewById(R.id.hikename);
+            mInfoText = itemView.findViewById(R.id.subTitle);
+            mHikeImage = itemView.findViewById(R.id.hikeimages);
+        }
+    }
+}
--- app/src/main/java/com/example/hikebuddy/MainActivity.java
+++ app/src/main/java/com/example/hikebuddy/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.hikebuddy;
 
 import android.os.Bundle;
-
+import android.content.res.TypedArray;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Hike> HikeData;
+    private HikeAdapter Adapter;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        HikeData = new ArrayList<>();
+        recyclerView = findViewById(R.id.rv_hike_list);
+        Adapter = new HikeAdapter(this, HikeData);
+        recyclerView.setAdapter(Adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //initializeData();
+        HikeData.add(new Hike("Koko head", "info on koko head", R.drawable.img_koko_head));
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -52,4 +69,35 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    /**
+     * Initialize the Hike data from resources.
+     *
+    private void initializeData() {
+        // Get the resources from the XML file.
+        String[] HikeList = getResources()
+                .getStringArray(R.array.hike_names);
+        String[] HikeInfo = getResources()
+                .getStringArray(R.array.hike_info);
+        TypedArray HikeImageResources = getResources()
+                .obtainTypedArray(R.array.hike_images);
+
+        // Clear the existing data (to avoid duplication).
+        HikeData.clear();
+
+        // Create the ArrayList of Hike objects with the titles and
+        // information about each Hike
+        for (int i = 0; i < HikeList.length; i++) {
+            HikeData.add(new Hike(HikeList[i], HikeInfo[i],
+                    HikeImageResources.getResourceId(i, 0)));
+        }
+
+        // Recycle the typed array.
+        HikeImageResources.recycle();
+
+        // Notify the adapter of the change.
+        Adapter.notifyDataSetChanged();
+    }
+     */
 }
