f09af9a9e59a0cbad4d205c6f6e0b549598a4ba8
Nathan Onaka <nateonaka@gmail.com>
2020-03-07 14:09:12 -1000

    Clickable Images and second Activity

---
+++ app/src/main/java/com/example/hikebuddy/HikeInfoActivity.java
@@ -0,0 +1,40 @@
+package com.example.hikebuddy;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.content.Context;
+import com.example.hikebuddy.R;
+import static android.view.KeyCharacterMap.load;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class HikeInfoActivity extends AppCompatActivity {
+
+    /**
+     * Initializes the activity, filling in the data from the Intent.
+     *
+     * @param savedInstanceState Contains information about the saved state
+     *                           of the activity.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hike_info_activity);
+
+        // Initialize the views.
+        TextView hikeTitle = findViewById(R.id.hikename);
+        ImageView hikeImage = findViewById(R.id.hikeimages);
+
+        // Set the text and image from the Intent extra.
+        hikeTitle.setText(getIntent().getStringExtra("title"));
+        hikeImage.setImageResource(getIntent().getIntExtra("image_resource",0));
+    }
+}
