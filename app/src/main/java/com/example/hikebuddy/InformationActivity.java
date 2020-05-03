eb60c4df6bb7ce50e0b31dcbfcc676cb1e45264c
chrisjna <35389960+chrisjna@users.noreply.github.com>
2020-05-02 16:48:07 -1000

    information

--- app/src/main/java/com/example/hikebuddy/InformationActivity.java
+++ app/src/main/java/com/example/hikebuddy/InformationActivity.java
@@ -1,8 +1,12 @@
 package com.example.hikebuddy;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import org.w3c.dom.Text;
 
 public class InformationActivity extends AppCompatActivity {
 
@@ -12,6 +16,9 @@ public class InformationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_information);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarHome);
         setSupportActionBar(toolbar);
+
+        TextView text = (TextView) findViewById(R.id.large_text_id);
+        text.setText(getText(R.string.large_text));
 
     }
 }
